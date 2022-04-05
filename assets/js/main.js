db47e66c8f6e94e7a4afc5c9124fae52170f450a
justin0077 <>
2022-04-05 16:02:49 +0200

    New sections added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
       navToggle = document.getElementById("nav-toggle"),
-      navClose = document.getElementById("nav-close");
+      navClose = document.getElementById("nav-close")
 
 if(navToggle) {
     navToggle.addEventListener("click", () => {
@@ -16,12 +16,51 @@ if(navClose) {
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+    const navMenu = document.getElementById("nav-menu");
+
+    navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction));
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById("header");
+
+    if(this.scrollY >= 50) {
+        header.classList.add("scroll-header");
+    } else {
+        header.classList.remove("scroll-header");
+    }
+}
+
+window.addEventListener("scroll", scrollHeader);
 
 
 /*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: "auto",
+    grabCursor: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            spaceBetween: 48,
+        }
+    }
+})
 
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
