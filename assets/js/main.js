81c84e63b8214174b85f8c053164bba23ffdb703
justin0077 <>
2022-04-05 11:28:57 +0200

    Added Header and Navigation

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close");
+
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== POPULAR SWIPER ===============*/
+
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+
+
+/* Link active featured */ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
